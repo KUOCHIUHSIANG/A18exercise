3a1d5378f6e1d647118d799e8a983616d3d82537
Jater <p833795p@gmail.com>
2021-09-06 17:21:36 +0800

    add handlebars and set routing for other tabs

--- app.js
+++ app.js
@@ -6,9 +6,22 @@ const exphbs = require('express-handlebars')
 
 app.engine('hbs', exphbs( {defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
 })
 
 app.listen(port, () => {
